6bb88a5924eab68637730bd133f87464b0071423
Patraxx <patrik.cederlund@gmail.com>
2024-09-24 14:54:26 +0200

    k

--- Pendax-hub/main/main.c
+++ Pendax-hub/main/main.c
@@ -6,9 +6,28 @@
 
 void device_command(void *pvParameters)
 {
+    while (1)
+    {
+        int socket = socket_create(TCP_SOCKET);
+        struct sockaddr_in server_address;
+        setup_address(&server_address, device_1_ip, DEVICE_PORT);    //PORT 1986
+        socket_bind(socket, &server_address, device_1_ip);
 
-    int socket = socket_create(TCP_SOCKET);
-    
+        int err = connect(socket, (struct sockaddr *)&server_address, sizeof(server_address));
+        if (err < 0)
+        {
+            ESP_LOGE("device_command", "Error occurred during connection to device 1");
+            close(socket);
+        }
+        else
+        {
+            ESP_LOGI("device_command", "Connected to device 1");
+            char *message = "TOF";
+            send(socket, message, strlen(message), 0);
+            ESP_LOGI("device_command", "Message sent to device 1");
+            close(socket);
+        }
+    }   
 }
 
 void app_main(void)
